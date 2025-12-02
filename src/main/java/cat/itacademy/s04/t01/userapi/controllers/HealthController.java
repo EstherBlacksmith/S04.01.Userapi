f3f5e59e5b9edc2ca443539a8ce682029e28d14e
Esther <githubEsther@gmail.com>
2025-12-02 16:54:05 +0100

    Feat: adding Json record

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public String healthy() {
-        return "OK";
+    public Status healthy() {
+        return new Status("OK");
     }
 }
