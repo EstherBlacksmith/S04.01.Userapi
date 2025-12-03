82916a6fb947d31a45423689febde362aa8a74ef
Esther <githubEsther@gmail.com>
2025-12-03 17:46:30 +0100

    Feat: new classes repositories

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/InMemoryUserRepository.java
@@ -0,0 +1,39 @@
+package cat.itacademy.s04.t01.userapi;
+
+import cat.itacademy.s04.t01.userapi.entities.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository{
+    static List<User> userList = new ArrayList<>();
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return false;
+    }
+}
